ead4b4315e78c5a2233f287688521ac2068d86f2
Mark Youngman <mark@youngman.info>
2019-08-16 08:20:01 +0100

    Trying to add xfce4 config

--- install_config.sh
+++ install_config.sh
@@ -12,6 +12,10 @@ ln -s $(pwd)/bash/.inputrc ~/.inputrc
 [ -f ~/.xprofile ] && mv ~/.xprofile ~/.xprofile$(date +%Y%m%d)
 ln -s $(pwd)/bash/.xprofile ~/.xprofile
 
+[ -L ~/.config/xfce4 ] && rm ~/.config/xfce4
+[ -d ~/.config/xfce4/ ] && mv ~/.config/xfce4 ~/.config/xfce4$(date +%Y%m%d)
+ln -s $(pwd)/xfce4 ~/.config/xfce4
+
 [ -L ~/.config/nvim ] && rm ~/.config/nvim
 [ -d ~/.config/nvim/ ] && mv ~/.config/nvim ~/.config/nvim$(date +%Y%m%d)
 ln -s $(pwd)/nvim ~/.config/nvim
@@ -21,17 +25,3 @@ nvim -c ":PlugClean|:PlugInstall|:qa"
 [ -L ~/.ideavimrc ] && rm ~/.ideavimrc
 [ -f ~/.ideavimrc ] && mv ~/.ideavimrc ~/.ideavimrc$(date +%Y%m%d)
 ln -s $(pwd)/intellij/.ideavimrc ~/.ideavimrc
-
-# And for root?
-#[ -L /root/.bashrc ] && rm /root/.bashrc
-#[ -f /root/.bashrc ] && mv /root/.bashrc /root/.bashrc$(date +%Y%m%d)
-#ln -s $(pwd)/bash/.bashrc /root/.bashrc
-#
-#[ -L /root/.inputrc ] && rm /root/.inputrc
-#[ -f /root/.inputrc ] && mv /root/.inputrc /root/.inputrc$(date +%Y%m%d)
-#ln -s $(pwd)/bash/.inputrc /root/.inputrc
-#
-# This incorrectly creates a symlink to nvim in the $(pwd)/nvim folder!
-#[ -L /root/.config/nvim ] && rm /root/.config/nvim
-#[ -d /root/.config/nvim/ ] && mv /root/.config/nvim /root/.config/nvim$(date +%Y%m%d)
-#ln -s $(pwd)/nvim /root/.config/nvim
