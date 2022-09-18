97cf7e756c008dd2a3a1d6332a625dd9952231be
Mark Youngman <mark@youngman.info>
2022-09-18 08:26:50 +0100

    Use packer for vim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,13 @@
 -- init.lua
+require "mark.plugins"
 require "mark.options"
 require "mark.keymaps"
 
 -- load old config
 --vim.cmd('source old_config.vim')
 --
---vim.fn['plug#begin']('~/.config/nvim/plugged')
-----vim.cmd [[Plug 'neoclide/coc.nvim', {'branch': 'release'}]]
---vim.cmd [[Plug 'scrooloose/nerdtree']]
---vim.cmd [[Plug 'easymotion/vim-easymotion']]
---vim.cmd [[Plug 'junegunn/fzf', { 'do':'./install --all' }]]
---vim.cmd [[Plug 'junegunn/fzf.vim']]
-----vim.cmd [[Plug 'HerringtonDarkholme/yats.vim']] -- TS highlighting
-----vim.cmd [[Plug 'andreyorst/SimpleClangFormat.vim']]
---vim.fn['plug#end']()
-
 vim.cmd('colorscheme desert')
 vim.cmd('syntax on')
 
+-- clear whitespace on save
 vim.cmd("autocmd BufWritePre * :%s/\\s\\+$//e")
