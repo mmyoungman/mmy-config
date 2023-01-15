2779b6aa2bfc6bcf5afcf9eea3ed0b8fdf1d2725
Mark Youngman <mark@youngman.info>
2023-01-15 16:06:33 +0000

    Small vim config fix

--- nvim/lua/mark/lsp/handlers.lua
+++ nvim/lua/mark/lsp/handlers.lua
@@ -107,7 +107,7 @@ local function lsp_keymaps(bufnr)
   )
   buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format { async = true }' ]]
 end
 
 M.on_attach = function(client, bufnr)
