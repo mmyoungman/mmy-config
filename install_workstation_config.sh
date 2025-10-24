c4f8b9b8768aa6293a3c7d09a2bec37b8ed2b162
Mark Youngman <mark@youngman.info>
2025-10-24 10:12:04 +0100

    Ansible: Pull bash script out of workstation.yml

---
+++ install_workstation_config.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+PLAYBOOK_DIR="$1"
+[ "$PLAYBOOK_DIR" = "" ] && echo "need playbook dir as an arg" && exit 1
+
+CONFIG_FILES_DIR="$PLAYBOOK_DIR/config_files"
+
+set -e  # exit on any error
+
+for file in .bashrc .inputrc .xprofile .ideavimrc; do
+  [ -L "$HOME/$file" ] && rm "$HOME/$file"
+  [ -f "$HOME/$file" ] && mv "$HOME/$file" "$HOME/$file$(date +%Y%m%d)"
+  ln -s $CONFIG_FILES_DIR/$file "$HOME/$file"
+
+  [ ! -L "$HOME/$file" ] && echo "symlink not found $HOME/$file" && exit 1
+  target=$(readlink -f "$HOME/$file")
+  expected=$CONFIG_FILES_DIR/$(basename "$HOME/$file")
+  [ "$target" != "$expected" ] && echo "sym link wrong $HOME/$file" && exit 1
+done
+
+[ -L $HOME/.config/Code/User/settings.json ] && rm $HOME/.config/Code/User/settings.json
+[ -f $HOME/.config/Code/User/settings.json ] && mv $HOME/.config/Code/User/settings.json $HOME/.config/Code/User/settings$(date +%Y%m%d).json
+[ -d $HOME/.config/Code/User ] && ln -s $CONFIG_FILES_DIR/vscode-settings.json $HOME/.config/Code/User/settings.json
+
+# @MarkFix arch specific...
+[ -L $HOME/.config/Code\ -\ OSS/User/settings.json ] && rm $HOME/.config/Code\ -\ OSS/User/settings.json
+[ -f $HOME/.config/Code\ -\ OSS/User/settings.json ] && mv $HOME/.config/Code\ -\ OSS/User/settings.json $HOME/.config/Code\ -\ OSS/User/settings$(date +%Y%m%d).json
+[ -d $HOME/.config/Code\ -\ OSS/User ] && ln -s $CONFIG_FILES_DIR/vscode-settings.json $HOME/.config/Code\ -\ OSS/User/settings.json
+
+[ -L $HOME/.config/nvim ] && rm $HOME/.config/nvim
+[ -d $HOME/.config/nvim ] && mv $HOME/.config/nvim $HOME/.config/nvim$(date +%Y%m%d)
+ln -s $CONFIG_FILES_DIR/nvim $HOME/.config/nvim
+[ ! -L "$HOME/.config/nvim" ] && echo "symlink not found $HOME/.config/nvm" && exit 1
+target=$(readlink -f "$HOME/.config/nvim")
+expected=$CONFIG_FILES_DIR/$(basename "$HOME/.config/nvim")
+[ "$target" != "$expected" ] && echo "sym link wrong $HOME/.config/nvim" && exit 1
+
+[ -L $HOME/.gitconfig ] && rm $HOME/.gitconfig
+[ -f $HOME/.gitconfig ] && [ ! -f $HOME/.gitconfig$(date +%Y%m%d) ] && mv $HOME/.gitconfig $HOME/.gitconfig$(date +%Y%m%d)
+[ -f $HOME/.gitconfig ] && rm $HOME/.gitconfig
+# we copy later using a template to obscure the email address
+
+# @MarkFix v0.12 has a built in plugin manager...
+# nvim --headless -c 'Lazy install' -c 'Lazy update' -c 'quitall'
+
+exit 0
