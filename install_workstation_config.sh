e3941ca21719d75f74fc32a64d4a716a3e8b3de4
Mark Youngman <mark@youngman.info>
2026-08-22 15:58:34 +0100

    Hypr: add input config

--- install_workstation_config.sh
+++ install_workstation_config.sh
@@ -15,6 +15,11 @@ for file in .bashrc .inputrc .xprofile .ideavimrc; do
   [ ! -L "$HOME/$file" ] && echo "symlink not found $HOME/$file" && exit 1
 done
 
+# hyprland
+[ -L $HOME/.config/hypr/input.lua ] && rm $HOME/.config/hypr/input.lua
+[ -f $HOME/.config/hypr/input.lua ] && mv $HOME/.config/hypr/input.lua $HOME/.config/hypr/input$(date +%Y%m%d).lua
+[ -d $HOME/.config/hypr ] && ln -s $CONFIG_FILES_DIR/hypr/input.lua $HOME/.config/hypr/input.lua
+
 [ -L $HOME/.config/Code/User/settings.json ] && rm $HOME/.config/Code/User/settings.json
 [ -f $HOME/.config/Code/User/settings.json ] && mv $HOME/.config/Code/User/settings.json $HOME/.config/Code/User/settings$(date +%Y%m%d).json
 [ -d $HOME/.config/Code/User ] && ln -s $CONFIG_FILES_DIR/vscode-settings.json $HOME/.config/Code/User/settings.json
